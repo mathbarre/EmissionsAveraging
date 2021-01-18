8f1ed4084e4b6b27fdea61470f5a7a9d7d6e5f9e
mathbarre <mathieu.barre83@gmail.com>
2021-01-18 18:36:39 +0100

    change logspace exampe

--- CH4Avg/examples/gpu/barycenter_sp5_gpu.py
+++ CH4Avg/examples/gpu/barycenter_sp5_gpu.py
@@ -15,7 +15,7 @@ basepath = '../../data/permian_wind_reprojected/'
 
 (A, dimx, dimy, wind, y_over_x_ratio) = load_data(
     basepath, quality_thr=1, iswind=(3, 4))
-A = preprocess(A[:100, :], normalize=(lambda x: x/np.nanmean(x)))
+A = preprocess(A[:, :], normalize=(lambda x: x/np.nanmean(x)))
 Tot = preprocess(A, to_2D=(dimx, dimy))
 
 # M is of shape shape dimx / dimy ^ 4
@@ -36,19 +36,19 @@ for i in range(Tot.shape[2]):
         separable=True, wind=wind[:, i],
         wind_factor=wind_factor)
 
-reg = 0.0015
+reg = 0.0025
 reg_m = 0.5
 
 start_time = time.time()
 G2D = barycenter_unbalanced_sinkhorn2D(
     Tot, Cx, Cy, reg, reg_m, weights=None,
     numItermax=100, stopThr=1e-4, verbose=True,
-    log=True, logspace=True, reg_K=1e-16)
+    log=True, logspace=False, reg_K=1e-16)
 print(time.time() - start_time, "s cpu")
 start_time = time.time()
 G2Dw = barycenter_unbalanced_sinkhorn2D_wind(
     Tot, Cxs, Cys, reg, reg_m, weights=None, numItermax=100,
-    stopThr=1e-4, verbose=True, log=True, logspace=True, reg_K=1e-16)
+    stopThr=1e-4, verbose=True, log=True, logspace=False, reg_K=1e-16)
 print(time.time() - start_time, "s cpu")
 
 Tot_gpu = cp.array(Tot)
@@ -59,12 +59,12 @@ start_time = time.time()
 G2D_gpu = barycenter_unbalanced_sinkhorn2D_gpu(
     Tot_gpu, Cx_gpu, Cy_gpu, reg, reg_m, weights=None,
     numItermax=300, stopThr=1e-4, verbose=True,
-    log=True, logspace=True, reg_K=1e-16)
+    log=True, logspace=False, reg_K=1e-16)
 print(time.time() - start_time, "s gpu")
 start_time = time.time()
 G2Dw_gpu = barycenter_unbalanced_sinkhorn2D_wind_gpu(
     Tot_gpu, Cxs_gpu, Cys_gpu, reg, reg_m, weights=None, numItermax=300,
-    stopThr=1e-4, verbose=True, log=True, logspace=True, reg_K=1e-16)
+    stopThr=1e-4, verbose=True, log=True, logspace=False, reg_K=1e-16)
 print(time.time() - start_time, "s gpu")
 # %%
 cm = 'OrRd'
